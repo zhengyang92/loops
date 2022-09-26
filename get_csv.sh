e45fc29283749d18f4b22517464c2f1861ebe87c
Zhengyang Liu <liuz@cs.utah.edu>
2022-09-26 22:41:32 +0000

    foo

--- get_csv.sh
+++ get_csv.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-for i in $(cat changed) ;  do  echo -n "$i," ; grep "Total uOps" loops.baseline.mca/$i.baseline.mca |  tr -dc '0-9'; echo -n "," ; grep "Total uOps" loops.minotaur.mca/$i.minotaur.mca |  tr -dc '0-9'  ; echo "" ; done
+for i in $(cat changed) ;  do  echo -n "$i," ; grep "Total Cycles" loops.baseline.mca/$i.baseline.mca |  tr -dc '0-9'; echo -n "," ; grep "Total Cycles" loops.minotaur.mca/$i.minotaur.mca |  tr -dc '0-9'  ; echo "" ; done
