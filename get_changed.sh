38f3192ec81084553c639800a7b293a39b4f2739
Zhengyang Liu <liuz@cs.utah.edu>
2023-02-07 03:21:55 +0000

    foo

--- get_changed.sh
+++ get_changed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e
-for i in $(cat fuvr) ; do cmp  loops.baseline.asm/$i.baseline.S loops.minotaur.asm/$i.minotaur.S > /dev/null || echo $i ; done
+for i in $(cat fuvr) ; do cmp  loops.baseline.asm/$i.S loops.minotaur.asm/$i.S > /dev/null || echo $i ; done
